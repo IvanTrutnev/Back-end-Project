1b2d3370b4d8cae755b8804b763e6adf9b8da983
Ivan Trutnev <trutnev_1996@mail.ru>
2023-04-26 19:19:50 +0300

    rest api practice

--- src/index.ts
+++ src/index.ts
@@ -3,8 +3,41 @@ import express, { Request, Response } from 'express';
 const app = express();
 const port = process.env.PORT || 3000;
 
+app.use(express.urlencoded());
+
+let products = [
+  { id: 1, title: 'Banana' },
+  { id: 2, title: 'Tomato' },
+];
+
 app.get('/', (req: Request, res: Response) => {
-  res.send('Hello World!');
+  res.send(products);
+});
+
+app.get('/:id', (req: Request, res: Response) => {
+  const { id } = req.params;
+  const product = products.find((p) => p.id === +id);
+
+  if (product) {
+    res.send(product);
+  } else {
+    res.send(404);
+  }
+});
+
+app.post('/', (req: Request, res: Response) => {
+  const { title } = req.body;
+  console.log(req.body);
+  console.log(title);
+
+  const newProduct = {
+    id: +new Date(),
+    title,
+  };
+
+  products.push(newProduct);
+
+  res.status(201).send(newProduct);
 });
 
 app.listen(port, () => {
